8fa83d72906c0db8634c4ebec103ed10fcbe4a3e
Adam Jacyno <jacynoadam@gmail.com>
2020-11-08 17:25:55 +0100

    feat: implement readArray function

--- src/start/Lab4.java
+++ src/start/Lab4.java
@@ -1,44 +1,75 @@
 package start;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab4 {
 
-	// input data from standard input, if there is not enough data fill
-	// the remaining elements with 0's
-	public static int[] readArray(int size) { }
+	static Scanner input = new Scanner(System.in);
 
-	public static int[] appendArrays(int [] a1, int [] a2) { }
+	public static int[] readArray(int size) {
+		int[] arr = new int[size];
+		String str = input.nextLine();
+		String[] strArr = str.split("\\s+");
+		for (int i = 0; i < size; i++) {
+			if (i < strArr.length) {
+				arr[i] = Integer.parseInt(strArr[i]);
+			} else {
+				arr[i] = 0;
+			}
+		}
+		for (int i = 0; i < size; i++) {
+			System.out.println(arr[i]);
+		}
+		return arr;
+	}
 
-	public int getMinimalElement(int [] inArr) { }
+	public static int[] appendArrays(int[] a1, int[] a2) {
+	}
 
-	public int[] getMinimalElements(int [] inArr) { }
+	public int getMinimalElement(int[] inArr) {
+	}
 
-	public int[] getGreaterThan(int[] inArr, int limit) { }
+	public int[] getMinimalElements(int[] inArr) {
+	}
 
-	public int[] getRange(int[] inArr, int lowerLimit, int upperLimit) { }
+	public int[] getGreaterThan(int[] inArr, int limit) {
+	}
 
-	public int elementCount(int[] inArr, int what2Look4) { }
+	public int[] getRange(int[] inArr, int lowerLimit, int upperLimit) {
+	}
+
+	public int elementCount(int[] inArr, int what2Look4) {
+	}
 
 	// result contains elements that are in exactlty one array
-	public static int[] uniqueElements(int [] a1, int [] a2) { }
+	public static int[] uniqueElements(int[] a1, int[] a2) {
+	}
 
 	// result contains elements that are in both arrays
-	public static int[] commonElements(int [] a1, int [] a2) { }
+	public static int[] commonElements(int[] a1, int[] a2) {
+	}
 
-	public static void fillWith(int[] inOutArr, int fillValue) { }
+	public static void fillWith(int[] inOutArr, int fillValue) {
+	}
 
 	// reverse elements on spot
-	public static void reverseMe(int[] inOutArr) { }
+	public static void reverseMe(int[] inOutArr) {
+	}
 
 	// return an array with elements in reversed order
-	public static int[] returnReversed(int[] inArr) { }
+	public static int[] returnReversed(int[] inArr) {
+	}
 
 	// …
-	public static void showArr(int [] arrIn) { }
+	public static void showArr(int[] arrIn) {
+	}
 
 	// return an array with minimal values from each row
-	public static int[] getMinimals(int[][] inArr) { }
+	public static int[] getMinimals(int[][] inArr) {
+	}
 
-	public static void main(String[] args) { 
-		
+	public static void main(String[] args) {
+		readArray(Integer.parseInt(input.nextLine()));
 	}
 }
