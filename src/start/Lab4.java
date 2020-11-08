7abce19da01b41482a2c2c5b2c4321278bf0a623
Adam Jacyno <jacynoadam@gmail.com>
2020-11-08 15:18:17 +0100

    add: create a class for Lab4 tasks

---
+++ src/start/Lab4.java
@@ -0,0 +1,44 @@
+package start;
+
+public class Lab4 {
+
+	// input data from standard input, if there is not enough data fill
+	// the remaining elements with 0's
+	public static int[] readArray(int size) { }
+
+	public static int[] appendArrays(int [] a1, int [] a2) { }
+
+	public int getMinimalElement(int [] inArr) { }
+
+	public int[] getMinimalElements(int [] inArr) { }
+
+	public int[] getGreaterThan(int[] inArr, int limit) { }
+
+	public int[] getRange(int[] inArr, int lowerLimit, int upperLimit) { }
+
+	public int elementCount(int[] inArr, int what2Look4) { }
+
+	// result contains elements that are in exactlty one array
+	public static int[] uniqueElements(int [] a1, int [] a2) { }
+
+	// result contains elements that are in both arrays
+	public static int[] commonElements(int [] a1, int [] a2) { }
+
+	public static void fillWith(int[] inOutArr, int fillValue) { }
+
+	// reverse elements on spot
+	public static void reverseMe(int[] inOutArr) { }
+
+	// return an array with elements in reversed order
+	public static int[] returnReversed(int[] inArr) { }
+
+	// …
+	public static void showArr(int [] arrIn) { }
+
+	// return an array with minimal values from each row
+	public static int[] getMinimals(int[][] inArr) { }
+
+	public static void main(String[] args) { 
+		
+	}
+}
