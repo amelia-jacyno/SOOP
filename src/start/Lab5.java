50105681f85f8f9ad19bfa72fb110008b0017c2f
Adam Jacyno <jacynoadam@gmail.com>
2020-11-17 23:38:55 +0100

    feat: add printArr, arrayListToPrimitive, implement toNumberArray and add tests

--- src/start/Lab5.java
+++ src/start/Lab5.java
@@ -1,11 +1,52 @@
 package start;
 
+import java.util.ArrayList;
+
 public class Lab5 {
 
-    static int[] toNumberArray(int value) {
-
+    static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                System.out.print("{");
+            }
+            System.out.print(arr[i]);
+            if (i != arr.length-1) {
+                System.out.print(", ");
+            } else {
+                System.out.print("}\n");
+            }
+        }
     }
 
+    static int[] arrayListToPrimitive(ArrayList<Integer> arr) {
+        int[] resultArr = new int[arr.size()];
+        for (int i = 0; i < resultArr.length; i++) {
+            resultArr[i] = arr.get(i);
+        }
+        return resultArr;
+    }
+
+    static int[] toNumberArray(int value) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        if (value > 0) {
+            while (value > 0) {
+                arr.add(0, value%10);
+                value = (value - value%10) / 10;
+            }
+        } else if (value < 0) {
+            value *= -1;
+            while (value > 0) {
+                arr.add(0, value%10);
+                value = (value - value%10) / 10;
+            }
+            arr.set(0, arr.get(0) * -1);
+        } else {
+            arr.add(0);
+        }
+        return arrayListToPrimitive(arr);
+    }
+
+    /*
     static int[] toNumberArray(String value) {
 
     }
@@ -16,9 +57,11 @@ public class Lab5 {
 
     static int[] multiplyNumberArrays(int[] arr1, int[] arr2) {
 
-    }
+    } */
 
     public static void main(String[] args) {
-
+        printArr(toNumberArray(-31415));
+        printArr(toNumberArray(27182));
+        printArr(toNumberArray(0));
     }
 }
