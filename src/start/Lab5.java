9ea70f94d0c3c3a13d5f80a63cfc8797d4dccf45
Adam Jacyno <jacynoadam@gmail.com>
2020-11-17 23:12:17 +0100

    feat: add Lab5 and create outline of functions

---
+++ src/start/Lab5.java
@@ -0,0 +1,24 @@
+package start;
+
+public class Lab5 {
+
+    static int[] toNumberArray(int value) {
+
+    }
+
+    static int[] toNumberArray(String value) {
+
+    }
+
+    static int[] addNumberArrays(int[] arr1, int[] arr2) {
+
+    }
+
+    static int[] multiplyNumberArrays(int[] arr1, int[] arr2) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
