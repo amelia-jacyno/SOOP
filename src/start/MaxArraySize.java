fac80cad697da09d74614e1628d71beb6735398e
Adam Jacyno <jacynoadam@gmail.com>
2020-11-19 14:53:25 +0100

    ref: change the type of array to byte for less memory usage and fix memory calculation

--- src/start/MaxArraySize.java
+++ src/start/MaxArraySize.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MaxArraySize {
     public static void main(String[] args) {
-        final int MAX_RAM = 8192;
-        int[] arr = new int[MAX_RAM/8*1000000];
+        final int MAX_RAM = 3076;
+        byte[] arr = new byte[MAX_RAM*1000000L > Integer.MAX_VALUE - 2 ? Integer.MAX_VALUE - 2 : MAX_RAM * 1000000];
         System.out.println(arr.length);
         Scanner sc = new Scanner(System.in);
         sc.next();
