25c63c8ce9184a8bef38f127be70c990f33944d0
Adam Jacyno <jacynoadam@gmail.com>
2020-11-19 01:00:43 +0100

    feat: implement addNumberArrays, subtractNumberArrays, isLarger, isNegative, reverseSign

--- src/start/Lab5.java
+++ src/start/Lab5.java
@@ -8,7 +8,7 @@ public class Lab5 {
         System.out.print("{");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
-            if (i != arr.length-1) {
+            if (i != arr.length - 1) {
                 System.out.print(", ");
             }
         }
@@ -23,18 +23,54 @@ public class Lab5 {
         return resultArr;
     }
 
+    static void reverseSign(int[] arr) {
+        if (arr.length > 0) arr[0] *= -1;
+    }
+
+    static boolean isNegative(int[] arr) {
+        return arr.length > 0 && arr[0] < 0;
+    }
+
+    static boolean isLarger(int[] arr1, int[] arr2) {
+        if (arr1.length > 0 && arr2.length > 0) {
+            if (!isNegative(arr1) && !isNegative(arr2)) {
+                if (arr1.length > arr2.length) return true;
+                if (arr1.length < arr2.length) return false;
+                else {
+                    for (int i = 0; i < arr1.length; i++) {
+                        if (arr1[i] > arr2[i]) return true;
+                    }
+                    return false;
+                }
+            }
+            if (!isNegative(arr1) && isNegative(arr2)) return true;
+            if (isNegative(arr1) && !isNegative(arr2)) return false;
+            if (isNegative(arr1) && isNegative(arr2)) {
+                if (arr1.length > arr2.length) return false;
+                if (arr1.length < arr2.length) return true;
+                else {
+                    for (int i = 0; i < arr1.length; i++) {
+                        if (arr1[i] > arr2[i]) return false;
+                    }
+                    return true;
+                }
+            }
+        } else return arr2.length == 0;
+        return false;
+    }
+
     static int[] toNumberArray(int value) {
         ArrayList<Integer> arr = new ArrayList<>();
         if (value > 0) {
             while (value > 0) {
-                arr.add(0, value%10);
-                value = (value - value%10) / 10;
+                arr.add(0, value % 10);
+                value = (value - value % 10) / 10;
             }
         } else if (value < 0) {
             value *= -1;
             while (value > 0) {
-                arr.add(0, value%10);
-                value = (value - value%10) / 10;
+                arr.add(0, value % 10);
+                value = (value - value % 10) / 10;
             }
             arr.set(0, arr.get(0) * -1);
         } else {
@@ -54,11 +90,89 @@ public class Lab5 {
         return toNumberArray(intValue);
     }
 
-    /*
-    static int[] addNumberArrays(int[] arr1, int[] arr2) {
-
+    static int[] subtractNumberArrays(int[] arr1, int[] arr2) {
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        int remainder = 0;
+        for (int i = 0; i < arr1.length || i < arr2.length || remainder < 0; i++) {
+            int tempSum = remainder;
+            if (i < arr1.length) tempSum += arr1[arr1.length - 1 - i];
+            if (i < arr2.length) tempSum -= arr2[arr2.length - 1 - i];
+            if (tempSum < 0) {
+                remainder = -1;
+                tempSum += 10;
+            } else {
+                remainder = 0;
+            }
+            resultArr.add(0, tempSum);
+        }
+        for (int i = 0; i < resultArr.size() && resultArr.size() > 1; i++) {
+            if (resultArr.get(i) == 0) {
+                resultArr.remove(i);
+                i--;
+            }
+            else break;
+        }
+        return arrayListToPrimitive(resultArr);
     }
 
+    static int[] addNumberArrays(int[] arr1, int[] arr2) {
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        if (isNegative(arr1) == isNegative(arr2)) {
+            int remainder = 0;
+            for (int i = 0; i < arr1.length || i < arr2.length || remainder > 0; i++) {
+                int tempSum = remainder;
+                if (i == arr1.length - 1) {
+                    if (isNegative(arr1)) tempSum -= arr1[0];
+                    else tempSum += arr1[0];
+                } else if (i < arr1.length) tempSum += arr1[arr1.length - 1 - i];
+                if (i == arr2.length - 1) {
+                    if (isNegative(arr2)) tempSum -= arr2[0];
+                    else tempSum += arr2[0];
+                } else if (i < arr2.length) tempSum += arr2[arr2.length - 1 - i];
+                if (tempSum >= 10) {
+                    remainder = 1;
+                    tempSum -= 10;
+                } else {
+                    remainder = 0;
+                }
+                resultArr.add(0, tempSum);
+            }
+        } else {
+            int[] tempArr;
+            boolean arr1Reversed = false;
+            boolean arr2Reversed = false;
+            if (isNegative(arr1)) {
+                reverseSign(arr1);
+                arr1Reversed = true;
+            }
+            if (isNegative(arr2)) {
+                reverseSign(arr2);
+                arr2Reversed = true;
+            }
+            if (isLarger(arr1, arr2)) {
+                tempArr = subtractNumberArrays(arr1, arr2);
+                if (arr1Reversed) reverseSign(tempArr);
+            } else {
+                tempArr = subtractNumberArrays(arr2, arr1);
+                if (arr2Reversed) reverseSign(tempArr);
+            }
+            if (arr1Reversed) reverseSign(arr1);
+            if (arr2Reversed) reverseSign(arr2);
+            return tempArr;
+        }
+        for (int i = 0; i < resultArr.size() && resultArr.size() > 1; i++) {
+            if (resultArr.get(i) == 0) {
+                resultArr.remove(i);
+                i--;
+            }
+            else break;
+        }
+        if (isNegative(arr1) == isNegative(arr2) && isNegative(arr1) && resultArr.size() > 0)
+            resultArr.set(0, resultArr.get(0) * -1);
+        return arrayListToPrimitive(resultArr);
+    }
+
+    /*
     static int[] multiplyNumberArrays(int[] arr1, int[] arr2) {
 
     } */
@@ -67,6 +181,7 @@ public class Lab5 {
         printArr(toNumberArray(-31415));
         printArr(toNumberArray(27182));
         printArr(toNumberArray(0));
+        System.out.println();
 
         printArr(toNumberArray("xd"));
         printArr(toNumberArray("-12ey3"));
@@ -75,5 +190,16 @@ public class Lab5 {
         printArr(toNumberArray("123"));
         printArr(toNumberArray("0"));
         printArr(toNumberArray(""));
+        System.out.println();
+
+        printArr(addNumberArrays(toNumberArray(-2137), toNumberArray(1000)));
+        printArr(addNumberArrays(toNumberArray(100), toNumberArray(-123)));
+        printArr(addNumberArrays(toNumberArray(123), toNumberArray(-123)));
+        printArr(addNumberArrays(toNumberArray(-123), toNumberArray(123)));
+        printArr(addNumberArrays(toNumberArray(-123), toNumberArray(0)));
+        printArr(addNumberArrays(toNumberArray(0), toNumberArray(123)));
+        printArr(addNumberArrays(toNumberArray(0), toNumberArray(0)));
+        printArr(addNumberArrays(toNumberArray(123), toNumberArray(123)));
+        printArr(addNumberArrays(toNumberArray(-123), toNumberArray(-123)));
     }
 }
