03e49efdbb4984a37ea8685d2c2fd5cf5a2714f0
Adam Jacyno <jacynoadam@gmail.com>
2020-11-19 12:42:55 +0100

    feat: implement factorial using iteration and recursion

--- src/start/Lab06.java
+++ src/start/Lab06.java
@@ -2,15 +2,28 @@ package start;
 
 public class Lab06 {
 
-    /*
     static long factorialIter(int arg) {
-
+        if (arg < 0) return -1;
+        long result = 1;
+        for (int i = 2; i <= arg; i++) {
+            result *= i;
+        }
+        return result;
     }
 
     static long factorialRecur(int arg) {
-
+        if (arg < 0) return -1;
+        return factorialRecur(arg, arg, 1);
     }
 
+    static long factorialRecur(int arg, int index, int result) {
+        if (index == 1 || index == 0) {
+            return result;
+        }
+        return factorialRecur(arg, index - 1, result * index);
+    }
+
+    /*
     static int greatestCommonDivisorIter(int a, int b) {
 
     }
@@ -28,6 +41,17 @@ public class Lab06 {
     } */
 
     public static void main(String[] args) {
+        System.out.println("Factorials using iteration:");
+        System.out.println(factorialIter(10));
+        System.out.println(factorialIter(5));
+        System.out.println(factorialIter(0));
+        System.out.println(factorialIter(-1));
+        System.out.println();
 
+        System.out.println("Factorials using recursion:");
+        System.out.println(factorialRecur(10));
+        System.out.println(factorialRecur(5));
+        System.out.println(factorialRecur(0));
+        System.out.println(factorialRecur(-1));
     }
 }
