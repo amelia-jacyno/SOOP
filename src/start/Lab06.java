ca83050f4ab6fa8f5b30cb7632c857f92e56d42d
Adam Jacyno <jacynoadam@gmail.com>
2020-11-19 12:26:32 +0100

    add: Lab06 with outline of functions for Task 1

---
+++ src/start/Lab06.java
@@ -0,0 +1,33 @@
+package start;
+
+public class Lab06 {
+
+    /*
+    static long factorialIter(int arg) {
+
+    }
+
+    static long factorialRecur(int arg) {
+
+    }
+
+    static int greatestCommonDivisorIter(int a, int b) {
+
+    }
+
+    static int greatestCommonDivisorRecur(int a, int b) {
+
+    }
+
+    static int sumPositiveElementsIter(int[] arr) {
+
+    }
+
+    static int sumPositiveElementsRecur(int[] arr) {
+
+    } */
+
+    public static void main(String[] args) {
+
+    }
+}
