480c78caa847e0417f582c10a59c85c237b754ef
Adam Jacyno <jacynoadam@gmail.com>
2020-11-21 19:35:02 +0100

    add: FileHelper with getFileExtension function from Guava

---
+++ src/helpers/FileHelper.java
@@ -0,0 +1,13 @@
+package helpers;
+
+import java.io.File;
+
+public class FileHelper {
+
+    public static String getFileExtension(File file) {
+        String fileName = file.getName();
+        int dotIndex = fileName.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
+    }
+
+}
