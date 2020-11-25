2cf4b08b6264e0c3c9623605b422da2bc45bf295
Adam Jacyno <jacynoadam@gmail.com>
2020-11-26 00:26:18 +0100

    add: files used for Lab06

---
+++ resources/txtFiles1/fakeFile.bat
@@ -0,0 +1,1 @@
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id velit eget lectus aliquam tempus vel sed eros. Integer ut erat dignissim, tincidunt dolor sit amet, maximus felis. Proin a tincidunt purus, nec imperdiet nibh.
