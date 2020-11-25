dcdb3d98524bcb2dc93331acebb6775c9c63ce8e
Adam Jacyno <jacynoadam@gmail.com>
2020-11-25 23:09:40 +0100

    feat: implement 3 Item constructors and data validation

--- src/classes/class07/Item.java
+++ src/classes/class07/Item.java
@@ -1,7 +1,27 @@
 package classes.class07;
 
 public class Item {
+    private static final int minQuantity = 1, maxQuantity = 20;
     private ProductType type;
-    private int quantity;
-    private String comment;
+    private int quantity = 1;
+    private String comment = "";
+
+    public Item(ProductType type, int quantity, String comment) {
+        this.type = type;
+        if (isQuantityValid(quantity)) this.quantity = quantity;
+        this.comment = comment;
+    }
+
+    public Item(ProductType type, int quantity) {
+        this.type = type;
+        if (isQuantityValid(quantity)) this.quantity = quantity;
+    }
+
+    public Item(ProductType type) {
+        this.type = type;
+    }
+
+    private boolean isQuantityValid(int quantity) {
+        return quantity >= minQuantity && quantity <= maxQuantity;
+    }
 }
