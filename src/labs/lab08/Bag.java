96c5803d0a4442668c9457a9a630d415711154c2
Adam Jacyno <jacynoadam@gmail.com>
2020-12-01 12:14:18 +0100

    feat: implement Bag constructor

--- src/labs/lab08/Bag.java
+++ src/labs/lab08/Bag.java
@@ -1,4 +1,15 @@
 package labs.lab08;
 
+import labs.lab07.Item;
+
 public class Bag {
+    private static final int minCapacity = 1;
+    private Item[] items;
+    private int totalCapacity, usedCapacity = 0;
+
+    public Bag(int totalCapacity) throws IllegalArgumentException {
+        if (totalCapacity < minCapacity) throw new IllegalArgumentException("Invalid totalCapacity: " + totalCapacity);
+        this.totalCapacity = totalCapacity;
+        items = new Item[totalCapacity];
+    }
 }
